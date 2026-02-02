a8b1564a3a2df5d9bf84cd35959cf17f761a7216
= <19tomek11@gmail.com>
2026-02-02 16:46:20 +0100

    licznik + cykl zycia

--- app/src/main/java/com/example/czaszyciaaplikacji/MainActivity.java
+++ app/src/main/java/com/example/czaszyciaaplikacji/MainActivity.java
@@ -1,24 +1,87 @@
 package com.example.czaszyciaaplikacji;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    int licznik = 0;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Log.i("CYKL Zycia", "uruchomiona metoda onCreate");
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        if(savedInstanceState != null){
+            licznik = savedInstanceState.getInt("LICZNIK");
+            textView.setText(""+licznik);
+        }
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        licznik++;
+                        textView.setText(""+licznik);
+                    }
+                }
+        );
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i("CYKL Zycia", "uruchomiona metode onSaveInstanceState");
+        outState.putInt("LICZNIK", licznik);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("CYKL Zycia", "uruchomiona metode onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("CYKL Zycia", "uruchomiona metoda onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("CYKL Zycia", "uruchomiona metoda onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("CYKL Zycia", "uruchomiona metoda onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("CYKL Zycia", "uruchomiona metoda onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("CYKL Zycia", "uruchomiona metoda onRestart");
     }
 }
